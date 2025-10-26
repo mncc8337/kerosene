3bd348cb4db52e63fcab1cdd64bd2f4e9f7f01c9
mncc8337 <mncc8327@gmail.com>
2025-10-26 20:26:10 +0700

    [pmmngr] fix minor bugs

--- kernel/src/mem/pmmngr.c
+++ kernel/src/mem/pmmngr.c
@@ -1,5 +1,7 @@
 #include "mem.h"
 
+#include "string.h"
+
 // bitmap, the length is hardcoded because memory size will not exceed 3GiB
 static uint32_t bitmap[24576]; // 3GiB * 1024*1024*1024 / 4096(block size) / 32 (size of uint32_t)
 static size_t used_block;
@@ -11,16 +13,23 @@ static void set_bit(uint32_t bit) {
 static void unset_bit(uint32_t bit) {
     bitmap[bit/32] &= ~(1 << (bit % 32));
 }
-// static bool test_bit(uint32_t bit) {
-//     return (bitmap[bit/32] >> (bit % 32)) & 1;
-// }
+static bool test_bit(uint32_t bit) {
+    return (bitmap[bit/32] >> (bit % 32)) & 1;
+}
 
 static int find_first_free_block() {
     for(unsigned i = 0; i < total_block/32; i++) {
         if(bitmap[i] == 0xffffffff) continue;
-        for(int j = 0; j < 32;j++) {
+        for(int j = 0; j < 32; j++) {
+            unsigned bit_index = i * 32 + j;
+            // stop checking if we're past the end of RAM
+            if(bit_index >= total_block)
+                return -1;
+            
             int bit = 1 << j;
-            if(!(bitmap[i] & bit)) return i * 32 + j;
+            if(!(bitmap[i] & bit)) {
+                return bit_index;
+            }
         }
     }
     return -1;
@@ -29,22 +38,29 @@ static int find_first_free(size_t frame_cnt) {
     if(frame_cnt == 0) return -1;
     if(frame_cnt == 1) return find_first_free_block();
 
-    for(unsigned int i = 0; i < total_block/32; i++) {
-        if(bitmap[i] == 0xfffffff) continue;
-        for(int j = 0; j < 32;j++) {
-            int bit = 1 << j;
-            if(bitmap[i] & bit) continue;
+    if(frame_cnt > total_block)
+        return -1;
 
-            for(uint32_t k = 1; k < frame_cnt; k++) {
-                if(bitmap[i] & (bit << k)) {
-                    // we know that this segment has not enough space so we can skip
-                    j += k;
+    for(uint32_t i = 0; i <= total_block - frame_cnt; i++) {
+        if(!test_bit(i)) {
+            // check the rest of the bits
+            uint32_t k = 1;
+            for(; k < frame_cnt; k++) {
+                if(test_bit(i + k)) {
+                    // this block is used, so our contiguous run is broken
                     break;
                 }
-                if(k == frame_cnt - 1) return i * 32 + j;
             }
+
+            if(k == frame_cnt)
+                return i;
+
+            // we know i+k is used.
+            // so the next possible free block is at i+k+1
+            i += k;
         }
     }
+
     return -1;
 }
 
@@ -52,17 +68,32 @@ static int find_first_free(size_t frame_cnt) {
 // must be run after done initializing regions
 void pmmngr_update_usage() {
     used_block = 0;
-    for(unsigned i = 0; i < total_block/32; i++) {
+    size_t num_chunks = (total_block + 31) / 32;
+    for(unsigned i = 0; i < num_chunks; i++) {
         if(bitmap[i] == 0xffffffff) {
-            used_block += 32;
-            continue;
+            // be careful not to count padding bits
+            int bits_in_this_chunk = 32;
+            // if this is the last chunk
+            if(i == num_chunks - 1) {
+                bits_in_this_chunk = total_block % 32;
+                if(bits_in_this_chunk == 0) bits_in_this_chunk = 32;
+            }
+            
+            if(bits_in_this_chunk == 32) {
+                 used_block += 32;
+                 continue;
+            }
         }
-        for(int j = 0; j < 32;j++) {
+
+        for(int j = 0; j < 32; j++) {
+            unsigned bit_index = i * 32 + j;
+            if(bit_index >= total_block)
+                break;
+
             int bit = 1 << j;
             if((bitmap[i] & bit)) used_block++;
         }
     }
-    if(used_block > total_block) used_block = total_block;
 }
 size_t pmmngr_get_size() {
     return total_block * MMNGR_PAGE_SIZE;
@@ -123,8 +154,10 @@ void pmmngr_free_block(void* base) {
 
     if(frame == 0) return;
 
-    unset_bit(frame);
-    used_block--;
+    if(test_bit(frame)) {
+        unset_bit(frame);
+        used_block--;
+    }
 }
 void pmmngr_free_multi_block(void* base, size_t cnt) {
     physical_addr_t addr = (physical_addr_t)base;
@@ -132,9 +165,12 @@ void pmmngr_free_multi_block(void* base, size_t cnt) {
 
     if(frame == 0) return;
 
-    for(uint32_t i = 0; i < cnt; i++)
-        unset_bit(frame+i);
-    used_block -= cnt;
+    for(uint32_t i = 0; i < cnt; i++) {
+        if(test_bit(frame + i)) {
+            unset_bit(frame + i);
+            used_block--;
+        }
+    }
 }
 
 void pmmngr_init(size_t size) {
@@ -142,6 +178,5 @@ void pmmngr_init(size_t size) {
 
     // assume that all memory are in use
     used_block = total_block;
-    for(unsigned i = 0; i < total_block / 32; i++)
-        bitmap[i] = 0xffffffff;
+    memset(bitmap, 0xFF, sizeof(bitmap));
 }
