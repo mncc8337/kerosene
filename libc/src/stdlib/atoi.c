9d3ba5d5f0329b4e84d96f30d51ed7db8afff815
mncc8337 <mncc8327@gmail.com>
2026-09-12 08:11:23 +0700

    [libc] sign support for atoi

--- libc/src/stdlib/atoi.c
+++ libc/src/stdlib/atoi.c
@@ -2,12 +2,18 @@
 
 int atoi(char* buff) {
     int ret = 0;
-
+    int sign = 1;
     char* p = buff;
+
+    if(*p == '-') {
+        sign = -1;
+        p++;
+    }
+
     while(*p != '\0') {
         ret = ret * 10 + (*p) - '0';
         p++;
     }
 
-    return ret;
+    return ret * sign;
 }
